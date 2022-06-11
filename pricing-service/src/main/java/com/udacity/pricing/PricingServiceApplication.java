f70cd3de3be74774dc31618f16c270ba63a8777c
ngocduy12 <duydang9902@gmail.com>
2022-06-11 12:55:39 +0700

    commit 11 06 2022 12:55PM

--- pricing-service/src/main/java/com/udacity/pricing/PricingServiceApplication.java
+++ pricing-service/src/main/java/com/udacity/pricing/PricingServiceApplication.java
@@ -3,11 +3,14 @@ package com.udacity.pricing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 /**
  * Creates a Spring Boot Application to run the Pricing Service.
  * TODO: Convert the application from a REST API to a microservice.
  */
 @SpringBootApplication
+//@EnableEurekaClient
+
 public class PricingServiceApplication {
 
     public static void main(String[] args) {
